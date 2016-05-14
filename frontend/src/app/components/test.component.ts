2a9921e5159d9d9ddbdf17a19a8ff65e7656fc44
Srijan Bajracharya <srijanbajracharya@lftechnology.com>
2016-05-14 09:41:33 +0545

    bootstrap css added

--- frontend/src/app/components/test.component.ts
+++ frontend/src/app/components/test.component.ts
@@ -7,6 +7,7 @@ import { DATEPICKER_DIRECTIVES} from 'ng2-bootstrap/ng2-bootstrap';
                 <div style="display:inline-block; min-height:290px;">
     <datepicker [(ngModel)]="date" showWeeks="true"></datepicker>
   </div>
+  <button type="button" class="btn btn-primary btn-sm">Send</button>
                 `,
     directives: [DATEPICKER_DIRECTIVES]
 })
