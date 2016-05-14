da2e5f2b8b0ee15580bd427e225548022b07b5b0
Srijan Bajracharya <srijanbajracharya@lftechnology.com>
2016-05-14 08:38:52 +0545

    bootstrap added

--- frontend/src/app/components/test.component.ts
+++ frontend/src/app/components/test.component.ts
@@ -2,7 +2,9 @@ import {Component} from '@angular/core';
 
 @Component({
     selector:'hello',
-    template:"<h2>My code works</h2>"
+    template:`<h2>My code works</h2>
+                <span class="glyphicon glyphicon-plus"></span>
+                `
 })
 export class Test{
     
