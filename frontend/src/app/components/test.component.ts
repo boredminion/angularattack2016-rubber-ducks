bed2c6e1c181e6176e4d10ceeef9b940f9364cc2
Srijan Bajracharya <srijanbajracharya@lftechnology.com>
2016-05-14 09:28:54 +0545

    added ng2-bootstrap

--- frontend/src/app/components/test.component.ts
+++ frontend/src/app/components/test.component.ts
@@ -1,10 +1,14 @@
 import {Component} from '@angular/core';
+import { DATEPICKER_DIRECTIVES} from 'ng2-bootstrap/ng2-bootstrap';
 
 @Component({
     selector:'hello',
     template:`<h2>My code works</h2>
-                <span class="glyphicon glyphicon-plus"></span>
-                `
+                <div style="display:inline-block; min-height:290px;">
+    <datepicker [(ngModel)]="date" showWeeks="true"></datepicker>
+  </div>
+                `,
+    directives: [DATEPICKER_DIRECTIVES]
 })
 export class Test{
     
