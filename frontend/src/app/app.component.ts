123d942178c82f6493572ff91eaf3f19c67be150
Srijan Bajracharya <srijanbajracharya@lftechnology.com>
2016-05-14 07:41:22 +0545

    folder structure added

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import '../../public/css/styles.css';
+import {Test} from "./components/test.component";
 @Component({
   selector: 'my-app',
   template: require('./app.component.html'),
-  styles: [require('./app.component.css')]
+  styles: [require('./app.component.css')],
+  directives: [Test]
 })
 export class AppComponent { }
 
---
+++ frontend/src/app/components/test.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'hello',
+    template:"<h2>My code works</h2>"
+})
+export class Test{
+    
+}
