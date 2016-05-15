fc6c5a52687c4cf8263047f953f64dedf9812688
Prakash Poudel <prakashpoudel@lftechnology.com>
2016-05-15 17:22:54 +0545

    checkpoint commmit

--- frontend/src/app/components/searchresult/searchresult.component.ts
+++ frontend/src/app/components/searchresult/searchresult.component.ts
@@ -14,26 +14,39 @@ export class SearchResultComponent {
 
     constructor(private albumCreateService:AlbumCreateService) {
     }
-    public errorMessage: string;
 
+    public errorMessage: string;
+    private submitted = false;
     public model:Album = {
-        id: '2',
+        id: '',
         name: "some name",
         description: "good description",
         user_id: '1',
-        albumCover: "https://myfavouriteoldthings.files.wordpress.com/2013/10/sphinx-rubber-duck1.jpg",
-        photos: []
+        albumCover: "",
+        photos: [
+            {url: "../../../../public/img/avatar.jpg"},
+            {url: "../../../../public/img/avatar.jpg"}
+        ]
     };
 
-    submitData() {
-        event.preventDefault();
+    onSubmit() {
+        debugger;
+        this.submitted = true;
         this.albumCreateService.addAlbum(this.model.name, this.model.description, this.model.user_id, this.model.albumCover, this.model.photos).subscribe(
             album => this.model = album,
             error =>  this.errorMessage = <any>error
         );
 
-        // debugger;
-        console.log("I am triggered")
-        console.log(this.model)
     }
+
+    submitData() {
+        // event.preventDefault();
+        // debugger;
+        // console.log("I am triggered")
+        // console.log(this.model)
+    }
+
+
+    // TODO: Remove this when we're done
+    get diagnostic() { return JSON.stringify(this.model); }
 }
